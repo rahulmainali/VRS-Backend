0787798ef17c345c4bd762767ce127d358f73d34
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2023-06-10 11:53:07 -0400

    indexcontroller.ts

---
+++ src/controller/booking/index.controller.ts
@@ -0,0 +1,6 @@
+
+const CREATE_BOOKING = require('./subController/booking')
+const GET_BOOKING = require('./subController/getBooking')
+const GET_RENTALS = require('./subController/getRentals')
+
+module.exports = {createBooking: CREATE_BOOKING, getBooking: GET_BOOKING, getRentals: GET_RENTALS}
